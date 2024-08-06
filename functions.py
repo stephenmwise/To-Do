9b3b927e3d89b31d082f9093a7ae61d2aad71185
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-06 15:13:26 -0400

    added FILEPATH

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 # 1 - Read file lines of file as list items
-
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of to-do items.
     """
     with open(filepath, "r") as file:
@@ -8,7 +8,7 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 # 2 - Write to file as list items
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """Write the to-do items list in the text file"""
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
