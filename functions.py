05cf0a9e5853ce672a281a221f99c68091f69864
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-07 18:57:32 -0400

    Added functionality to app, including add and complete checkboxes

--- functions.py
+++ functions.py
@@ -4,8 +4,8 @@ FILEPATH = "todos.txt"
 def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of to-do items.
     """
-    with open(filepath, "r") as file:
-        todos_local = file.readlines()
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
     return todos_local
 
 
